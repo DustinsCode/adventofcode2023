26a3c877cb74f421fa22037d5fbbebabfc319711
Dustin Thurston <dthurston1996@gmail.com>
2023-12-02 17:23:08 -0500

    day 1 part 2 done

--- day1/day1.py
+++ day1/day1.py
@@ -1,21 +1,67 @@
 import re
 
 
-total = 0
+number_dict = {
+    "one":   "1",
+    "two":   "2",
+    "three": "3",
+    "four":  "4",
+    "five":  "5",
+    "six":   "6",
+    "seven": "7",
+    "eight": "8",
+    "nine":  "9",
+}
 
-with open("./input.txt") as f:
-    lines = f.readlines()
+def get_number_from_substring(s: str):
+    for key in number_dict.keys():
+            if key in s:
+                return number_dict[key]
+    return None
 
-    for line in lines:
-        numbers = re.sub("[a-z]+", "", line.strip())
+def find_first_digit(line: str):
+    start_str = ""
+    for char in line:
+        if(char.isdigit()):
+            return char
+        
+        start_str += char
+        result = get_number_from_substring(start_str)
+        if result != None:
+            return result
+ 
+def find_last_digit(line: str):
+    end_str = ""
+    line_reversed = line[::-1]
+    for char in line_reversed:
+        if(char.isdigit()):
+            return char
+        
+        end_str = char + end_str
+        result = get_number_from_substring(end_str)
+        if result != None:
+            return result
 
-        if(len(numbers) == 1):
-            numbers = f"{numbers}{numbers}"
-        else:
-            numbers = f"{numbers[0]}{numbers[len(numbers) - 1]}"
+def main():
+    total = 0
 
-        total += int(numbers)
-    
-    print(total)
+    with open("./input.txt") as f:
+        lines = f.readlines()
 
-f.close()
+        for line in lines:
+            
+            first_digit = 0
+            last_digit = 0
+            
+            first_digit = find_first_digit(line.strip())
+            last_digit = find_last_digit(line)
+           
+            total += int(first_digit + last_digit)
+        
+        print(total)
+
+    f.close()
+
+
+if __name__ == '__main__':
+    main()
